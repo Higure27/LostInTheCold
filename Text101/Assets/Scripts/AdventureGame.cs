6b6c8d67c9c1ab896561ce7515e628c1e60dd655
Kevan Rahav <kevan.or@gmail.com>
2019-02-17 20:34:43 -0700

    Commiting Font, classes and scriptable objects assets

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var availableStates = state.GetAvailableStates();
+        if(Input.GetKeyDown(KeyCode.Q)|| Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = availableStates[0];
+            textComponent.text = state.GetStateStoryText();
+        }
+        else if(availableStates.Length>1 && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = availableStates[1];
+            textComponent.text = state.GetStateStoryText();
+        }
+        else if (availableStates.Length > 2 && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = availableStates[2];
+            textComponent.text = state.GetStateStoryText();
+        }
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Allows to create "State" scriptable object from the menu 
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    //Text area size 10 lines and goes up to 14 before user needs to scroll in the inspector
+    [TextArea(10,14)][SerializeField] string StoryText;
+    //Story options the player can choose  from
+    [SerializeField] State[] availableStates;
+
+    public string GetStateStoryText()
+    {
+        return StoryText;
+    }
+
+    public State[] GetAvailableStates()
+    {
+        return availableStates;
+    }
+}
